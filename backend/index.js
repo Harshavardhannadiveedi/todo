7daac86f64e81ebd68a679aec7b6e10ec5252d67
HARSHAVARDHAN NADIVEEDI <161990167+Harshavardhannadiveedi@users.noreply.github.com>
2025-01-18 15:29:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 
 const corsOptions = {
-    origin: 'http://localhost:5173', 
+    origin: 'https://todoolist-fsd.netlify.app/', 
     credentials: true,
 };
 
